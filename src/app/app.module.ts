9119122e1732582bea9ce5f1c26f0492d7a68c0f
Anthony Garera <Gareraanthony@gmail.com>
2021-07-07 20:33:12 -0400

    Added Custom theme and new nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { NavigationComponent } from './navigation/navigation.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +28,13 @@ import { NavigationComponent } from './navigation/navigation.component';
     AppRoutingModule,
     ScullyLibModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
